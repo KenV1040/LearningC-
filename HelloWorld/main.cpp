f95664e29ed89a2babea994e57ce7212819b1f39
Ken <kenvu@protonmail.com>
2020-09-30 21:41:38 +0100

    initial commit

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -3,9 +3,10 @@
 
 int main() 
 {
+	// The << are just overloaded operators that are basically functions
 	std::cout << "Hello world" << std::endl;
 	std::cin.get();
 
-
+	// Will assume you are returning 0 if you don't specify it
 	return 0;
 }
