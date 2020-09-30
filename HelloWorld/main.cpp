83aac3e0650cb375a5ae11fc3262cb28970195ce
Ken <kenvu@protonmail.com>
2020-09-30 21:48:24 +0100

    Added more comments

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 // Anything with a has is a pre-processor statement
-
+// Gets evaluated before we compile the file
 int main() 
 {
 	// The << are just overloaded operators that are basically functions
